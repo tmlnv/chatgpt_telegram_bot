36acf501eaaad01522264adf7a8b3acaa0f50910
tmlnv <tmlnv@protonmail.com>
2024-03-03 21:32:48 +0300

    Updated chatgpt model version

--- bot/chatgpt.py
+++ bot/chatgpt.py
@@ -24,7 +24,7 @@ with open('bot/chat_modes.json', 'r') as file:
 
 
 class ChatGPT:
-    def __init__(self, model="gpt-3.5-turbo-16k"):
+    def __init__(self, model="gpt-3.5-turbo"):
         self.model = model
 
     async def send_message_stream(
